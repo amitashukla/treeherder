d80e6acfa2b56a9f7d13dabeae8eec348b6d00f5
Cameron Dawson <cdawson@mozilla.com>
2018-08-28 15:35:13 -0700

    Add generic ErrorBoundary

--- ui/job-view/PushList.jsx
+++ ui/job-view/PushList.jsx
@@ -13,6 +13,7 @@ import PushLoadErrors from './PushLoadErrors';
 import { thEvents } from '../js/constants';
 import JobModel from '../models/job';
 import PushModel from '../models/push';
+import ErrorBoundary from '../shared/ErrorBoundary';
 
 export default class PushList extends React.Component {
 
@@ -276,15 +277,20 @@ export default class PushList extends React.Component {
       <div>
         {jobsReady && <span className="hidden ready" />}
         {repoName && pushList.map(push => (
-          <Push
-            push={push}
-            isLoggedIn={isLoggedIn || false}
-            currentRepo={currentRepo}
-            isStaff={isStaff}
-            repoName={repoName}
-            $injector={$injector}
+          <ErrorBoundary
+            errorClasses="pl-2 border-top border-bottom border-dark d-block"
+            message={`Error on push with revision ${push.revision}: `}
             key={push.id}
-          />
+          >
+            <Push
+              push={push}
+              isLoggedIn={isLoggedIn || false}
+              currentRepo={currentRepo}
+              isStaff={isStaff}
+              repoName={repoName}
+              $injector={$injector}
+            />
+          </ErrorBoundary>
         ))}
         {loadingPushes &&
           <div
